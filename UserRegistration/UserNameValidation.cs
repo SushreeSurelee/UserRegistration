fe28712dc7527cfbd0576c6786fe0a8ce97e2dde
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-13 02:51:37 +0530

    [Sushree | Added functionality to validate password which satifying rule 4]

--- UserRegistration/UserNameValidation.cs
+++ UserRegistration/UserNameValidation.cs
@@ -42,7 +42,7 @@ namespace UserRegistration
         }
         public void ValidatePassword(string password)
         {
-            string pattern = "(?=.*?[A-Z])(?=.*?[0-9])[0-9a-zA-Z#?!@$%^&*-]{8,}";
+            string pattern = "(?=.*[A-Z])(?=.*[0-9])(?=.*[#?!@$%^&*-])[0-9a-zA-Z#?!@$%^&*-]{8,}";
             TestRegex(password, pattern);
         }
     }
